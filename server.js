dfc37b2ff999d2541897c54c7084689076103a5f
Qhub <qdaman90@gmail.com>
2020-01-20 17:02:21 -0600

    testing deployment

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ const port = process.env.PORT || 5000;
 
 const consumer_key = process.env.API_KEY;
 const consumer_secret = process.env.SECRET_API_KEY;
-const access_token = process.env.ACCESS_TOKEN;
-
 
 let token = "";
 
@@ -28,14 +26,6 @@ const config = {
   data: "grant_type=client_credentials"
 };
 
-const userConfig = {
-  url: "https://api.twitter.com/1.1/statuses/update.json",
-  method: "post",
-  headers: {
-
-  }
-}
-
 axios
   .request(config)
   .then(function(res) {
@@ -48,7 +38,7 @@ axios
 app.post("/search/tweets", (req, res) => {
   // res.send(Elon_Musk)
 
-  let keyword  = req.body.tweet;
+  let keyword = req.body.tweet;
 
   axios
     .get(
@@ -66,10 +56,9 @@ app.post("/search/tweets", (req, res) => {
 });
 
 app.post("/search/users", (req, res) => {
-
   let keyword = req.body.user.toLowerCase();
 
-    axios
+  axios
     .get(
       `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${keyword}&count=6`,
       { headers: { Authorization: "Bearer " + token } }
@@ -82,13 +71,11 @@ app.post("/search/users", (req, res) => {
       // handle error
       console.log(error);
     });
-  
 });
 app.post("/search/comedy", (req, res) => {
-
   let keyword = req.body.username.toLowerCase();
 
-    axios
+  axios
     .get(
       `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${keyword}&count=6`,
       { headers: { Authorization: "Bearer " + token } }
@@ -101,46 +88,23 @@ app.post("/search/comedy", (req, res) => {
       // handle error
       console.log(error);
     });
-  
 });
 
-// app.get("/users", (req, res) => {
-//   // res.send(Elon_Musk)
-// });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+//Static file declaration
+app.use(express.static(path.join(__dirname, "lurkers-twitter/build")));
 
-// function randomString(length, chars) {
-  //   var result = '';
-  //   for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
-  //   return result;
-  // } 
-  // // res.send(Elon_Musk)
-  
-  // let keyword = req.body.user.toLowerCase();
-  
-  // let nonce = randomString(32,'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
-  
-  // let time = Math.round((new Date()).getTime() / 1000.0);
-  
-  // axios
-  //   .get(
-  //     `https://api.twitter.com/1.1/users/search.json?q=${keyword}&result_type=popular&count=6`,
-  //     { headers: { Authorization: `OAuth oauth_consumer_key=${consumer_key},` +
-  //     'oauth_signature_method="HMAC-SHA1",' +
-  //     'oauth_timestamp='+time +
-  //     'oauth_nonce='+nonce +
-  //     'oauth_version="1.0",' +
-  //     `oauth_token=${access_token},`+
-  //     'oauth_signature='+token } }
-  //   )
-  //   .then(function(response) {
-  //     // handle success
-  //     console.log(response.data)
-  //     // res.send(response.data);
-  //   })
-  //   .catch(function(error) {
-  //     // handle error
-  //     console.log(error);
-  //   });
+//production mode
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "lurkers-twitter/build")));
+  app.get("*", (req, res) => {
+    res.sendfile(path.join((__dirname = "lurkers-twitter/build/index.html")));
+  });
+}
+//build mode 
+app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
+
+//start server 
+app.listen(port, (req, res) => {  console.log( `server listening on port: ${port}`);})
+
 
